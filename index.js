9490b45aab892f1305fc184c7bf9f0952fab87c3
Puru573 <purukumar.p@gmail.com>
2022-07-09 21:44:32 +0530

    let and const learn

--- index.js
+++ index.js
@@ -31,3 +31,28 @@ console.log(b);
 
 var num=500;
 console.log(`half of ${num} is ${num/2}`);
+
+let l =200;
+console.log(l);
+
+// let l="200"; // Identifier 'l' has already been declared
+// console.log(l);
+// we can reinitialize a variable but we can't redeclare it
+l=200;
+console.log(l);
+
+//for loop
+//for prime number
+
+var num = 10;
+for(var i=1;i<num;i++){
+    if(i%2==0){
+        console.log("number is even");
+    }
+}
+const c=2;
+console.log(c);
+
+//na hi redeclare na hi reintialize
+c=4;
+console.log("hey"); //Assignment to constant variable.
