761cfade5b0b09793114addeb835807f48bdd4cc
Puru573 <purukumar.p@gmail.com>
2022-07-09 16:57:21 +0530

    backtick explained

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ var t=true;
 var f=false;
 console.log(t);
 console.log(f);
+
+var str='a';
+console.log(typeof str);
+str="how you doin? \ni am fine .how about you?";
+console.log(str);
+
+var b=`hy hope you guys are also 
+cool healthy`;
+console.log(b);
+
+var num=500;
+console.log(`half of ${num} is ${num/2}`);
