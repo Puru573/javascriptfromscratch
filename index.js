5357af488096ff684e7736377f66477dc4ebef05
Puru573 <purukumar.p@gmail.com>
2022-07-09 16:36:21 +0530

    first code of js

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var a;
+a=10;
+console.log(typeof a);
+console.log(a);
+
+// "",' ',''
+//double quotes,single quotes,backtick
+
+a='puru';
+console.log(typeof a);
+
+a=null;
+console.log(typeof a);
+console.log(a);
+
+//boolean
+var t=true;
+var f=false;
+console.log(t);
+console.log(f);
