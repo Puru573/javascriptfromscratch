5f65ead7107c187aa0ef0f25ffe6f34f7701fa19
Puru573 <purukumar.p@gmail.com>
2022-07-12 14:47:59 +0530

    hoisting in function expression

---
+++ hoisting.js
@@ -0,0 +1,23 @@
+// console.log(a);
+// greet();
+
+// var a=10;
+// function greet(){
+//     console.log("hello pepcoders");
+// }
+// // console.log(a);
+
+
+console.log(a);
+greet();
+
+var a=10;
+// this is a normal function
+function greet(){
+    console.log("helloo pepcoders");
+}
+// this is a function expression
+var hello= function(){
+    console.log("you guys rock");
+}
+hello();
