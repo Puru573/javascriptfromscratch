5bf93780867f6d17507db56e1b1291ed7c0aa66d
Puru573 <purukumar.p@gmail.com>
2022-07-14 13:53:57 +0530

    int,var,cont

--- scopes.js
+++ scopes.js
@@ -1,9 +1,26 @@
-function outer(){
-    inner();
-    function inner(){
-        console.log(b);
-    }
-}
-var b=20;
-outer();
-console.log(b);
+// function outer(){
+//     inner();
+//     function inner(){
+//         console.log(b);
+//     }
+// }
+// var b=20;
+// outer();
+// console.log(b);
+// var add=function(x){
+//     function inner (y){
+//         function innermost (z){
+//             console.log(x,y,z);
+//             return x+y+z;
+//         }
+//         innermost(6);
+
+//     }
+//     inner(5);
+// }
+// add(4);
+
+// function sumValues(x,y,z){
+//     return x+y+z;
+// }
+// console.log(sumValues([...[1,2,3]]));
