6ddf042f1c4203bf60ba61c4f0bd5fe57e411bcb
Puru573 <purukumar.p@gmail.com>
2022-07-14 14:47:31 +0530

    block scope

--- int,var,const.js
+++ int,var,const.js
@@ -1,4 +1,16 @@
+// console.log(a);
+// var a=10;
+// let b=100;
+// console.log(b);
+var a=100;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 console.log(a);
-var a=10;
-let b=100;
 console.log(b);
